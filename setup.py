e7732374461bff887ad552dfe16863fb3e6f79d9
Kyle Holohan <kholohan@gmail.com>
2016-08-03 09:27:44 -0400

    First commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='py3example',
+      version='0.1',
+      description='Python 3 Example Project',
+      url='http://github.com/kholohan/py3example',
+      author='Kyle Holohan',
+      author_email='kholohan@gmail.com',
+      license='MIT',
+      packages=['py3example'],
+      zip_safe=True)
